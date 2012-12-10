ab3a66cf12a5c87255bdc55bcb771d0cef26041b
Jon Branham <arthur.branham@gmail.com>
2012-12-09 22:06:01 -0500

    Trying out push to remotely tracked branch

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("Hello World!");	
 		System.out.println("Date:"+new java.util.Date());
+		System.out.println(System.currentTimeMillis());
 	}
 }
